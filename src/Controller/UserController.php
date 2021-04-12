bfcffb2c1e3c0267b1ad1e335d37811e2eafe47f
lphilippe-source <philippelmr@gmail.com>
2021-04-12 15:18:57 +0200

    feat: :recycle: JsonDispatch service
    refactored json serialization for Controllers

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,12 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\HttpFoundation\Response;
+use App\Controller\Services\JsonDispatch;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
@@ -16,20 +12,11 @@ class UserController extends AbstractController
     /**
      * @Route("/blog/user", name="user")
      */
-    public function index(): Response
+    public function index()
     {
         $repo = $this->getDoctrine()->getRepository(User::class);
         $res = $repo->findAll();
-
-        $encoder = new JsonEncoder();
-        $defaultContext = [
-            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
-            return $object->getId();
-            }
-        ];
-        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
-        $serializer = new Serializer([$normalizer], [$encoder]);
-
-        return new Response($serializer->serialize($res, 'json'));
+        $json = new JsonDispatch($res);
+        return $json->getResponse();
     }
 }
